5adcd04a988ea3006d533f0c86c59c7bf120a5ed
danielneil <you@example.com>
2021-05-27 14:14:52 +1000

    just added the placeholder comment

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # exit when any command fails
 set -e
 
+echo "We are not quite ready yet! :)"
+exit 0;
 ##################################################
 # Run the playbook.
 ansible-playbook ./ansible/site.yml -i ./ansible/hosts
