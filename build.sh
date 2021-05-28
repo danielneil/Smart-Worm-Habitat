8f9e88eb39b0575920d8b3ec18615d4e256978ed
danielneil <you@example.com>
2021-05-28 11:25:47 +1000

    added nagios and httpd roles

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ InstallDeps "ansible"
 # Start ssh and enable it at boot.
 sudo systemctl enable ssh && sudo systemctl start ssh
 
-echo "We are not quite ready yet! :)"
-exit 0;
+#echo "We are not quite ready yet! :)"
+#exit 0;
 ##################################################
 # Run the playbook.
 ansible-playbook ./ansible/site.yml -i ./ansible/hosts
